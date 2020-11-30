e4a70c2bd2044d501e82f86b54ed77e439d54838
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-11-30 03:31:49 -0300

    simplify the navButtons to be created from a array

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NavButton } from './nav-button';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+	selector: 'app-top-bar',
+	templateUrl: './top-bar.component.html',
+	styleUrls: ['./top-bar.component.scss'],
 })
 export class TopBarComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	// Definimos todos os botoes de navegacao no header do site
+	navButtons: NavButton[] = [
+		{ title: 'Comunidade', route: '/comunidade' },
+		{ title: 'Cursos', route: '/cursos' },
+		{ title: 'Artigos', route: '/artigos' },
+		{ title: 'O Cll', route: '/apresentacao' },
+		{ title: 'Contato', route: '/Contato' },
+	];
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
